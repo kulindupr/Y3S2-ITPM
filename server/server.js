241e0b6b729f7dfcca43d6de3a6e4b7d8d5113c5
Asela Wanigabadu <kavindaasela18@gmail.com>
2025-04-29 15:35:50 +0530

    connect mongo db

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import cors from 'cors'
 import'dotenv/config';
+import connectDB from './config/db.js';
 
 //Initialize Express
 const app=express();
+await connectDB();
 
 //middleware3
 app.use(cors());
@@ -19,5 +21,5 @@ const PORT=process.env.PORT || 5000;
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
     
-    
+
 })
