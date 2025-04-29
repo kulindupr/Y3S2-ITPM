74f942a7a75aa81dd18747813794cfffe0ac0000
Asela Wanigabadu <kavindaasela18@gmail.com>
2025-04-30 00:52:49 +0530

    backend

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
+import'./config/instrument.js';
+
 import express from 'express'
 import cors from 'cors'
 import'dotenv/config';
 import connectDB from './config/db.js';
+import * as Sentry from "@sentry/node";
+import { celeryWebhook } from './controllers/webhooks.js';
 
 //Initialize Express
 const app=express();
@@ -14,9 +18,15 @@ app.use(express.json());
 //route
 app.get('/',(req,res)=>res.send('API Working!'));
 
+app.get("/debug-sentry", function mainHandler(req, res) {
+    throw new Error("My first Sentry error!");
+  });
+  app.post('/webhook', celeryWebhook) 
 
 //port
 const PORT=process.env.PORT || 5000;
+
+Sentry.setupExpressErrorHandler(app);
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
